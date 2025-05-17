4c0a301b51f4f85860901a9e84f614d0a2b5e207
Hyun-EG <codiee@naver.com>
2025-05-17 19:20:20 +0900

    Add: 홈 컴포넌트 추가 및 루트 페이지 연결

---
+++ app/(pages)/home/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="w-full min-h-screen flex flex-col">
+      <header>헤더</header>
+      <main className="flex-1">메인</main>
+      <footer>푸터</footer>
+    </section>
+  );
+};
+
+export default Home;
