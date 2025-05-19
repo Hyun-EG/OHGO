2f34a08381af393b96d08ee9447a5332348739e9
seonghyun <codiee@naver.com>
2025-05-19 21:16:54 +0900

    Debug: 터보팩이랑 Babel 충돌해결 Babel 테스트 설정 분리

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,14 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": [
+      "babel-jest",
+      { configFile: "./babel.config.test.js" },
+    ],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
+  testPathIgnorePatterns: ["/node_modules/", "/babel.config.test.js"],
 };
