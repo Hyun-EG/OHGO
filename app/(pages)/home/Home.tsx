9ae5d1694172c298b629d240b6a31ce8d7eb6c2d
seonghyun <codiee@naver.com>
2025-05-19 19:09:53 +0900

    Docs: 홈 구성 변경

--- app/(pages)/home/Home.tsx
+++ app/(pages)/home/Home.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
-    <section className="w-full min-h-screen flex flex-col">
-      <header>헤더</header>
+    <section className="w-full h-full flex flex-col">
       <main className="flex-1">메인</main>
-      <footer>푸터</footer>
     </section>
   );
 };
